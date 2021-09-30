7176ee1160902a31441e809bc493a42e4f5a5908
bakirmaksumov <30586761+bakirmaksumov@users.noreply.github.com>
2021-09-30 17:34:57 +0500

    changes in education

---
+++ EducationApplication/Common/PdfConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using TheArtOfDev.HtmlRenderer.PdfSharp;
+
+namespace EducationApplication.Common
+{
+    public static class PdfConverter
+    {
+        public static Byte[] PdfSharpConverter(string htmlContent)
+        {
+            Byte[] result = null;
+            using (var memory = new MemoryStream())
+            {
+                var config = new PdfGenerateConfig();
+                config.PageSize = PdfSharp.PageSize.A4;
+                config.SetMargins(30);
+
+                var pdf = PdfGenerator.GeneratePdf(htmlContent, config);
+                pdf.Save(memory);
+                result = memory.ToArray();
+            }
+            return result;
+        }
+    }
+}
